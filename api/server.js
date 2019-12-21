745339faa8339c0ee3595d56a3f1408bb5359762
Brendan Schatz <brendanschatz97@gmail.com>
2019-12-21 18:30:35 -0500

    started making my knex files

--- api/server.js
+++ api/server.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const server = express();
 
 server.use(helmet());
+server.use(morgan())
 server.use(express.json());
 server.use(cors());
 
 
 server.get("/", (req, res) => {
-    res.send("WE ARE UP!");
+    res.send("Now we're cooking!!!!");
 });
 
 module.exports = server;
