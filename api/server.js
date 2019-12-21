aee9556db58fbc4a905e0cb73705458bfa08c3d1
Brendan Schatz <brendanschatz97@gmail.com>
2019-12-21 16:26:12 -0500

    got a index and server page up server officially running!

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+
+server.get("/", (req, res) => {
+    res.send("WE ARE UP!");
+});
+
+module.exports = server;
