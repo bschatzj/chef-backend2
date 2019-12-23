5f36118c307a1636dfd9019cb272456f41c0da7d
Brendan Schatz <brendanschatz97@gmail.com>
2019-12-23 18:02:25 -0500

    cleaned up my structure

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const morgan = require("morgan");
+const authRoute = require("./auth/auth-router");
+const recipeRoute = require("./recipes/recipe-router");
 
 const server = express();
 
@@ -9,6 +11,9 @@ server.use(helmet());
 server.use(morgan())
 server.use(express.json());
 server.use(cors());
+
+server.use("/api/login", authRoute)
+server.use("api/recipes", recipeRoute)
 
 
 server.get("/", (req, res) => {
