c429013438b5d25382ebf24a59d5f178fb89a643
Brendan Schatz <brendanschatz97@gmail.com>
2020-01-07 18:39:48 -0500

    removed not nullable from a lot

--- data/migrations/20191227234329_newUsers.js
+++ data/migrations/20191227234329_newUsers.js
@@ -7,15 +7,12 @@ exports.up = function (knex) {
             .unique();
         table.string("password")
             .notNullable();
-        table.string("full_name")
-            .notNullable();
-        table.string("email", 128)
-            .notNullable();
+        table.string("full_name");
+        table.string("email", 128);
         table.string("user_picture")
         .defaultTo("https://static9.depositphotos.com/1575949/1203/v/950/depositphotos_12034707-stock-illustration-smiley-icon.jpg");
         table.string("phone");
-        table.string("Location", 128)
-            .notNullable();
+        table.string("Location", 128);
         table.string("Bio")
     });
 };
